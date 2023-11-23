fdf4b1b4132ca807724005cb5860be0a02aa6ae0
Christian Sosa <christiansosa77@gmail.com>
2023-11-23 14:45:34 -0300

    Firts Commit

---
+++ src/routers/views/index.router.js
@@ -0,0 +1,25 @@
+import { Router } from 'express';
+
+const router = Router();
+
+
+const publicRouters = (req, res, next) => {
+  if (req.session.user) {
+    return res.redirect('/products');
+  }
+  next();
+}
+
+router.get('/', (req, res)=>{
+  res.redirect('/login')
+})
+
+router.get('/login', publicRouters, (req, res) => {
+  res.render('login', { title: 'Login' });
+});
+
+router.get('/register', publicRouters, (req, res) => {
+  res.render('register', { title: 'Register' });
+});
+
+export default router;
